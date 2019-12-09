2aef40db267b6f42109ac59e284931d8c98c12fe
Lars van der Ham <lars@vanderham.email>
2019-12-09 21:03:26 +0100

    Disable ingress as it doesn't work with React

--- rootfs/etc/init/run.sh
+++ rootfs/etc/init/run.sh
@@ -3,7 +3,7 @@
 if bashio::config.true 'ssl'; 
 then
   # Setup ssl in nginx
-  sed -i 's#%%portandmode%%#8080 ssl http2#g' /etc/nginx/nginx.conf
+  sed -i 's#%%portandmode%%#80 ssl http2#g' /etc/nginx/nginx.conf
 
   CERTFILE=$(bashio::config 'certfile')
   KEYFILE=$(bashio::config 'keyfile')
@@ -12,7 +12,7 @@ then
   sed -i "s#%%certificatekeyfile%%#${KEYFILE}#g" /etc/nginx/nginx.conf
 else
   # Setup http ports in nginx
-  sed -i 's#%%portandmode%%#8080 default#g' /etc/nginx/nginx.conf
+  sed -i 's#%%portandmode%%#80 default#g' /etc/nginx/nginx.conf
   sed -i 's#ssl_certificate /ssl/%%certificatefile%%;##g' /etc/nginx/nginx.conf
   sed -i 's#ssl_certificate_key /ssl/%%certificatekeyfile%%;##g' /etc/nginx/nginx.conf
 fi
